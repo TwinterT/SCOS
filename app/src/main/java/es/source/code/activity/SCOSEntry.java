89b1e4b693734611400c4adbc5a302498529b1c4
TwinterT <1012179010@qq.com>
2018-09-18 20:06:55 +0800

    E1 first time

---
+++ app/src/main/java/es/source/code/activity/SCOSEntry.java
@@ -0,0 +1,13 @@
+package es.source.code.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SCOSEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry);
+    }
+}
